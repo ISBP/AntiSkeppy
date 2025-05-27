1ed1281046132c67e7b9857022348abb215a7629
PBSI <125078014+ISBP@users.noreply.github.com>
2025-05-26 20:50:20 -0700

    Added self mode to mute chat

--- src/main/java/xyz/pbsi/antiSkeppy/MuteChat.java
+++ src/main/java/xyz/pbsi/antiSkeppy/MuteChat.java
@@ -21,6 +21,7 @@ public class MuteChat implements CommandExecutor, Listener, TabExecutor {
     private boolean chat = true;
     private boolean staff = false;
     private boolean self = false;
+    private Player selfplayer = null;
     private AntiSkeppy plugin;
     public final String bypass = "mc.bypass";
     public final String staffBypass = "mc.staff";
@@ -37,17 +38,23 @@ public class MuteChat implements CommandExecutor, Listener, TabExecutor {
     public void onAsyncPlayerChat(AsyncPlayerChatEvent event)
     {
         Player player = event.getPlayer();
-        if(!player.hasPermission(bypass) && !chat)
+        if(!player.hasPermission(bypass) && !player.hasPermission(staffBypass) && !chat && !staff && !self)
         {
             event.setCancelled(true);
             player.sendMessage("§c§lMutechat:§r§f Chat is currently muted!");
 
         }
-        if(!player.hasPermission(staffBypass) && !chat && staff)
+        if(!player.hasPermission(staffBypass) && !chat && staff && !self)
         {
             event.setCancelled(true);
             player.sendMessage("§c§lMutechat:§r§f Chat is currently restricted to §cstaff!");
         }
+        if(player != selfplayer && self && !chat && !staff)
+        {
+            event.setCancelled(true);
+            player.sendMessage("§c§lMutechat:§r§f Chat is currently restricted to §c"+selfplayer.getName()+"!");
+        }
+
     }
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
@@ -66,9 +73,11 @@ public class MuteChat implements CommandExecutor, Listener, TabExecutor {
             if (commandSender.hasPermission(admin)) {
                 if (staff) {
                     staff = false;
+                    self = false;
                     commandSender.sendMessage("§c§lMutechat:§r§f Set mode to §cNORMAL");
                 } else {
                     staff = true;
+                    self = false;
                     commandSender.sendMessage("§c§lMutechat:§r§f Set mode to §cSTAFF");
                 }
                 return true;
@@ -82,14 +91,19 @@ public class MuteChat implements CommandExecutor, Listener, TabExecutor {
 
         if (strings[0].equalsIgnoreCase("Self")) {
             if (commandSender.hasPermission(admin)) {
-                if (self) {
-                    self = false;
-                    commandSender.sendMessage("§c§lMutechat:§r§f Set mode to §cNORMAL");
-                } else {
-                    staff = true;
-                    commandSender.sendMessage("§c§lMutechat:§r§f Set mode to §cSELF");
+                if(commandSender instanceof Player) {
+                    selfplayer = (Player) commandSender;
+                    if (self) {
+                        self = false;
+                        commandSender.sendMessage("§c§lMutechat:§r§f Set mode to §cNORMAL");
+                        staff = false;
+                    } else {
+                        self = true;
+                        commandSender.sendMessage("§c§lMutechat:§r§f Set mode to §cSELF");
+                        staff = false;
+                    }
+                    return true;
                 }
-                return true;
             }
             else{
                 commandSender.sendMessage("§cThis requires admin permission!");
@@ -103,7 +117,7 @@ public class MuteChat implements CommandExecutor, Listener, TabExecutor {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if(args.length == 1)
-            return Arrays.asList("Staff");
+            return Arrays.asList("STAFF", "SELF");
 
         return new ArrayList<>();
     }
